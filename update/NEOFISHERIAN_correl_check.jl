0e201df939081a9f81dad92ad3edec7bfb49c2f5
Matyas, Farkas <matyas.farkas@ecb.europa.eu>
2025-05-09 12:41:48 -0400

    Neofisherian simulations in update done - each 40th draw from MH chain simulated 100 times to compute HVD of individual shocks to r_obs and r* in Del Negro et al.

--- update/NEOFISHERIAN_correl_check.jl
+++ update/NEOFISHERIAN_correl_check.jl
@@ -35,7 +35,7 @@ m <= Setting(:n_mh_blocks, 10) # Do 10 blocks
 
 # m <= Setting(:use_population_forecast, false) # Population forecast not available as data to turn off
 m <= Setting(:forecast_block_size, 5) # adjust block size to run on small number of estimations
-do_not_run_estimation = false
+# do_not_run_estimation = false
 #############
 # Estimation
 #############
@@ -48,30 +48,33 @@ do_not_run_estimation = false
 # but we turn this feature off by setting the keyword `check_empty_columns = false`.
 # Warnings will be still thrown after calling load_data indicating which columns
 # are empty. However, estimate will still run when data is missing.
-@time begin
-# Run estimation
-if do_not_run_estimation
-    # Start from full sample mode
-   mode_file = rawpath(m, "estimate", "paramsmode.h5")
-   #mode_file = replace(mode_file, "ss20", "ss18")
-   DSGE.update!(m, h5read(mode_file, "params"))
-   hessian_file = rawpath(m, "estimate", "hessian.h5")
-   DSGE.specify_hessian!(m, hessian_file)
+# @time begin
+# # Run estimation
+# if do_not_run_estimation
+#     # Start from full sample mode
+#    mode_file = rawpath(m, "estimate", "paramsmode.h5")
+#    #mode_file = replace(mode_file, "ss20", "ss18")
+#    DSGE.update!(m, h5read(mode_file, "params"))
+#    hessian_file = rawpath(m, "estimate", "hessian.h5")
+#    DSGE.specify_hessian!(m, hessian_file)
 
-else
-    df = load_data(m, try_disk = false, check_empty_columns = false, summary_statistics = :none)
-        # Define the COVID19 date range
-        start_date = Date(2020, 3, 31)
-        end_date = Date(2020, 9, 30) 
-        # Replace rows within the date range with missing values
-        allowmissing!(df)
-        df[(df.date .>= start_date) .& (df.date .<= end_date), 2:end] .= missing
-    data = df_to_matrix(m, df)
-    DSGE.estimate(m, data)
+# else
+#     df = load_data(m, try_disk = false, check_empty_columns = false, summary_statistics = :none)
+#         # Define the COVID19 date range
+#         start_date = Date(2020, 3, 31)
+#         end_date = Date(2020, 9, 30) 
+#         # Replace rows within the date range with missing values
+#         allowmissing!(df)
+#         df[(df.date .>= start_date) .& (df.date .<= end_date), 2:end] .= missing
+#     data = df_to_matrix(m, df)
+#     DSGE.estimate(m, data)
 
-end
-end
-# Load posterior draws
+# end
+# end
+
+df = load_data(m, try_disk = false, check_empty_columns = false, summary_statistics = :none)
+# # Load posterior draws
+
 params_draws = load_draws(m, :full)
 params_mode = load_draws(m, :mode)
 shock_labels = [key for (key, _) in sort(collect(m.exogenous_shocks), by = x -> x[2])]
@@ -98,11 +101,10 @@ cor_sim = Matrix{Float64}(undef, simnum,drawnum)
 robs_sim = Array{Float64}(undef, n_periods,simnum,drawnum);
 rstar_sim = Array{Float64}(undef, n_periods,simnum,drawnum);
 ϵ_sim = Array{Float64}(undef,29,262,simnum,drawnum);
-
 drawi=1
 
 for drawloop in 1:40:4000
-    params = params_draws[kkk,:]
+    params = params_draws[drawloop,:]
 
     DSGE.update!(m, params)
     system = compute_system(m, tvis = false)
@@ -114,7 +116,7 @@ for drawloop in 1:40:4000
     
 
     for simi = 1:simnum
-                sim_data = DSGE.simulate_observables(m; burnin=500, n_periods=size(df, 1))'
+                # sim_data = DSGE.simulate_observables(m; burnin=500, n_periods=size(df, 1))'
                 ϵ_draw = rand(DegenerateMvNormal(zeros(29), sqrt.(QQ)), 500 + size(df, 1))
 
                 s, ϵ = DSGE.simulate_states(TTT,RRR,CCC,QQ; burnin =500,n_periods = 262,ϵ=ϵ_draw)
@@ -149,6 +151,58 @@ plot(1:262,[rstar_sim[:,100,100] ,  robs_sim[:,100,100]],title="NY-FED simulatio
 savefig( "obsnominalrate_vs_5yforwardrealrate.pdf")   # saves the plot from p as a .pdf vector graphic
 
 CSV.write("update/simulated_correlation.csv",DataFrame(reshape(cor_sim,10000)',:auto))
-
-
 CSV.write("update/simulated_correlation_description.csv",describe(DataFrame((cor_sim),:auto)))
+# Now the shock decomposition
+        drawi=1
+        robs_HVD = Array{Float64}(undef,262,29,simnum,drawnum);
+        rstar_HVD = Array{Float64}(undef,262,29,simnum,drawnum);
+
+        for simi = 1:simnum
+            drawi=1
+            for drawloop in 1:40:4000
+            params = params_draws[drawloop,:]
+            DSGE.update!(m, params)
+            system = compute_system(m, tvis = false)
+            stateHVD, obsHVD , pseudoHVD = shock_decompositions(system, 1, ϵ_sim[:,:,simi,drawi], 1, 262)
+            robs_HVD[:,:,simi,drawi] =obsHVD[ m.observables[:obs_nominalrate],:,:]
+            rstar_HVD[:,:,simi,drawi] =pseudoHVD[ m.pseudo_observables[:Forward5YearRealNaturalRate],:,:]
+            # println("Posterior draw " * string(drawi) * " done...")
+            drawi = drawi +1
+            end
+            println("HVD simulation draw " * string(simi) * " done...")
+
+        end
+
+robs_HVD_towrite = reshape(robs_HVD, 262, 29, 100 * 100)
+rstar_HVD_towrite = reshape(rstar_HVD, 262, 29, 100 * 100)
+
+hvdcorr = Array{Float64}(undef,10000, 29);
+
+for simi = 1:10000
+    for shocki = 1:29
+    hvdcorr[simi,shocki] = cor(robs_HVD_towrite[:,shocki,simi],rstar_HVD_towrite[:,shocki,simi])
+    end
+end
+
+mean_robs_HVD_df_towrite = DataFrame(mean(robs_HVD_towrite,dims=3)[:,:,1],collect(keys(m.exogenous_shocks)))
+mean_rstar_HVD_df_towrite = DataFrame(mean(rstar_HVD_towrite,dims=3)[:,:,1],collect(keys(m.exogenous_shocks)))
+
+CSV.write("update/simulated_HVD_robs.csv",mean_robs_HVD_df_towrite)
+CSV.write("update/simulated_HVD_rstar.csv",mean_rstar_HVD_df_towrite)
+
+corr_summary = describe(DataFrame(collect(keys(m.exogenous_shocks))[1] => vec(hvdcorr[:,1])))
+
+for shocki =1:29
+    try
+    CSV.write("update/simulated_HVD_corr_"* string(collect(keys(m.exogenous_shocks))[shocki]) *".csv",describe(DataFrame(collect(keys(m.exogenous_shocks))[shocki] => vec(hvdcorr[:,shocki]))))
+    newrow = describe(DataFrame(collect(keys(m.exogenous_shocks))[shocki] => vec(hvdcorr[:,shocki])))
+    corr_summary = vcat(corr_summary, newrow)
+
+    catch
+
+        println("Structural shock " * string(collect(keys(m.exogenous_shocks))[shocki]) * " skipped as it had been switched off...")
+    end
+end
+
+
+CSV.write("update/simulated_HVD_corr_allshocks.csv",(corr_summary))
