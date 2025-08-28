006e69ae92e73e54a361b6183679aec116496dd7
Mátyás Farkas <matyi.farkas@gmail.com>
2025-08-28 16:42:24 -0400

    Package for Brookings mode

--- dsge/temp.jl
+++ dsge/temp.jl
@@ -1,20 +1,19 @@
-using DSGE, ClusterManagers, HDF5
+using DSGE, ClusterManagers, HDF5, Plots
 
 ##########################################################################################
 ## SETUP
 ##########################################################################################
-## THIS MODEL FILTERS THE FULL POSTERIOR OF DEL NEGRO ET AL WITH THE OIS DATA PROVIDED 
-# FOR THE SAMPLE UNTIL 2023Q4.
 
 # What do you want to do?
 run_estimation     = false 
 run_modal_forecast = true 
-run_full_forecast  = false   
+run_full_forecast  = false
 
 # Initialize model object
 # Note that the default for m1010 uses 6 anticipated shocks
 m = Model1010("ss20")
 
+# Settings for data, paths, etc.
 dataroot = joinpath(dirname(@__FILE__()), "input_data")
 saveroot = dirname(@__FILE__())
 m <= DSGE.Setting(:dataroot, dataroot, "Input data directory path")
@@ -28,8 +27,43 @@ m <= DSGE.Setting(:reoptimize, true)
 m <= DSGE.Setting(:calculate_hessian, true)
 
 # Settings for forecast dates
-m <= DSGE.Setting(:date_forecast_start,  quartertodate("2025-Q3"))
-m <= DSGE.Setting(:date_conditional_end, quartertodate("2025-Q3"))
+m <= DSGE.Setting(:date_forecast_start,  quartertodate("2024-Q4"))
+m <= DSGE.Setting(:date_conditional_end, quartertodate("2024-Q4"))
+
+m <= DSGE.Setting(:forecast_block_size,  50)
+nworkers = 20
+addprocsfcn = addprocs_sge # choose to work with your scheduler; see ClusterManagers.jl
+
+df = load_data(m; check_empty_columns = false)
+    output_vars = Vector{Symbol}(undef,0)
+    do_histforecast= true
+    do_shockdecs   = true
+    if do_histforecast
+        # Write data to create historical and forecast output
+        output_vars = vcat(output_vars, [:histpseudo, :histobs, :histstdshocks,
+                                         :hist4qpseudo, :hist4qobs, :histutpseudo,
+                                         :forecastpseudo, :forecastobs, :forecastutpseudo,
+                                         :forecast4qpseudo, :forecast4qobs, :forecaststdshocks])
+    end
+
+    if do_shockdecs
+        # Shock decompositions of forecasts
+        output_vars = vcat(output_vars, [:dettrendobs, :dettrendpseudo, :trendobs,
+                                         :trendpseudo, :shockdecpseudo, :shockdecobs])
+    end
+        usual_model_forecast(m, :mode, :none, output_vars,     forecast_string = "",                         density_bands = [.5, .6, .68, .7, .8, .9],                         check_empty_columns = false)
+    sections = [:estimation, :forecast]
+output_vars = [:forecastobs, :forecastpseudo,:shockdecobs, :shockdecpseudo]
+
+
+
+    plot_standard_model_packet(m, :mode, :none, output_vars,
+                               forecast_string = "",
+                               sections = sections)
+
+                                   write_standard_model_packet(m, :mode, :none, output_vars,
+                                sections = sections, forecast_string = "")
+    moment_tables(m)
 
 
 ##########################################################################################
@@ -83,13 +117,12 @@ if run_modal_forecast || run_full_forecast
 
         # compute means and bands
         compute_meansbands(m, :mode, cond_type, output_vars)
+
+
                 # print history means and bands tables to csv
-                table_vars = [:ExAnteRealRate, :Forward5YearRealRate, :Forward10YearRealRate,
-                :RealNaturalRate, :Forward5YearRealNaturalRate,
-                :Forward10YearRealNaturalRate, :Forward20YearRealNaturalRate,
-                :Forward30YearRealNaturalRate]
+                table_vars = [:obs_nominalrate,:obs_gdp,:obs_longrate]
   
-                write_meansbands_tables_all(m, :mode, cond_type, [:histpseudo], forecast_string = forecast_string,
+                write_meansbands_tables_all(m, :mode, cond_type, [:histobs], forecast_string = forecast_string,
                               vars = table_vars)
 
     end
