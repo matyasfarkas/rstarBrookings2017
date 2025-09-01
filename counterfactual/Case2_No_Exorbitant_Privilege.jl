0ecbf98cde08518319f2081396ccf47827aca143
matyasfarkas <matyi.farkas@gmail.com>
2025-09-01 18:04:51 -0400

    Exorbitant Privilege Done

--- counterfactual/Case2_No_Exorbitant_Privilege.jl
+++ counterfactual/Case2_No_Exorbitant_Privilege.jl
@@ -1,111 +1,61 @@
-using DSGE, Dates, DataFrames,OrderedCollections, Dates,HDF5, CSV, JLD2, FileIO, Statistics, ModelConstructors
-# using Nullables, DataFrames, OrderedCollections, Dates,HDF5, CSV, JLD2, FileIO, Statistics
+using DSGE, ClusterManagers, HDF5, Plots, StatsPlots
+using DataFrames, CSV
 
-#############################
-# Written by: Matyas Farkas, IMF
-# Based on the run_default.jl script of DSGE.jl and the test forecast driver file
-# 25 August 2025
-#############################
+##########################################################################################
+## SETUP
+##########################################################################################
+##############
+# Load the EA model with FG 
+##############
+use_FG_in_EA  = true  # set to false to load the EA model without FG shocks
 
-#############################
-# To use:
-# Just run in the Julia REPL
-# include("run_default.jl")
-# Note that the estimation
-# step will take 2-3 hours.
-#############################
+
+# Initialize model object
+# Note that the default for m1010 uses 6 anticipated shocks
+m = Model1010("ss20")
 
 # DSGE.Settings for data, paths, etc.
 mypath = @__DIR__
-idx = findlast(c -> c == '\\', mypath)
+idx = findlast(c -> c == '/', mypath)
 basepath = mypath[1:idx]
 dataroot = joinpath(basepath, "dsge", "input_data")
 saveroot = joinpath(basepath, "dsge")
 
 
-##############
-# Model Setup
-##############
-# Instantiate the FRBNY DSGE model object
-m = Model1012("ss20")
-
 m <= DSGE.Setting(:dataroot, dataroot, "Input data directory path")
 m <= DSGE.Setting(:saveroot, saveroot, "Output data directory path")
-m <= DSGE.Setting(:data_vintage, "250324")
-    df = DSGE.load_data(m)
-    data = df_to_matrix(m, df)
-    estimate(m, data; verbose=:low)
-
-df = load_data(m, try_disk = false, check_empty_columns = false, summary_statistics = :none)
-
-do_not_run_estimation = true
-#############
-# Estimation
-#############
-# Reoptimize parameter vector, compute Hessian at mode, and full posterior
-# parameter sampling.
-#
-# Note some columns will have missing data because not all our data
-# is publicly available. By default, `load_data` will error
-# if any of the columns in the loaded DataFrame is empty,
-# but we turn this feature off by setting the keyword `check_empty_columns = false`.
-# Warnings will be still thrown after calling load_data indicating which columns
-# are empty. However, estimate will still run when data is missing.
-@time begin
-# Run estimation
-if do_not_run_estimation
-    # Start from full sample mode
-   mode_file = rawpath(m, "estimate", "paramsmode.h5")
-   #mode_file = replace(mode_file, "ss20", "ss18")
-   DSGE.update!(m, h5read(mode_file, "params"))
-   hessian_file = rawpath(m, "estimate", "hessian.h5")
-   DSGE.specify_hessian!(m, hessian_file)
-
+if use_FG_in_EA 
+    m <= DSGE.Setting(:data_vintage, "250113")
 else
-    df = load_data(m, try_disk = false, check_empty_columns = false, summary_statistics = :none)
-        # Define the COVID19 date range
-        start_date = Date(2020, 3, 31)
-        end_date = Date(2020, 9, 30) 
-        # Replace rows within the date range with missing values
-        allowmissing!(df)
-        df[(df.date .>= start_date) .& (df.date .<= end_date), 2:end] .= missing
-    data = df_to_matrix(m, df)
-    DSGE.estimate(m, data)
-
+    m <= DSGE.Setting(:data_vintage, "250114")
 end
-end
+m <= DSGE.Setting(:reoptimize, false)
+m <= DSGE.Setting(:calculate_hessian, false)
+m <= DSGE.Setting(:date_mainsample_start,  quartertodate("1970-Q3"))
+m <= DSGE.Setting(:date_presample_start,  quartertodate("1970-Q2"))
 
-@time begin
-# Run estimation
-if do_not_run_estimation
-    # Start from full sample mode
-   mode_file = rawpath(m, "estimate", "paramsmode.h5")
-   #mode_file = replace(mode_file, "ss20", "ss18")
-   DSGE.update!(m, h5read(mode_file, "params"))
-   hessian_file = rawpath(m, "estimate", "hessian.h5")
-   DSGE.specify_hessian!(m, hessian_file)
+# Settings for forecast dates
+m <= DSGE.Setting(:date_forecast_start,  quartertodate("2024-Q3"))
+m <= DSGE.Setting(:date_conditional_end, quartertodate("2024-Q3"))
 
+
+df = load_data(m; check_empty_columns = false)
+if use_FG_in_EA
+mode_file = joinpath("dsge/output_data/m1010/ss20/estimate/raw/" ,  "paramsmode_vint=250113.h5")
 else
-    df = load_data(m, try_disk = false, check_empty_columns = false, summary_statistics = :none)
-        # Define the COVID19 date range
-        start_date = Date(2020, 3, 31)
-        end_date = Date(2020, 9, 30) 
-        # Replace rows within the date range with missing values
-        allowmissing!(df)
-        df[(df.date .>= start_date) .& (df.date .<= end_date), 2:end] .= missing
-    data = df_to_matrix(m, df)
-    DSGE.estimate(m, data)
-
+mode_file = joinpath("dsge/output_data/m1010/ss20/estimate/raw/" ,  "paramsmode_vint=250114.h5")
 end
-end
-params_mode = load_draws(m, :mode)
+specify_mode!(m, mode_file)
+system = DSGE.compute_system(m)
 
-DSGE.update!(m, params_mode)
+states = Dict{Symbol, Matrix{Float64}}()
+shocks = Dict{Symbol, Matrix{Float64}}()
+pseudo = Dict{Symbol, Matrix{Float64}}()
+
 shock_labels = [key for (key, _) in sort(collect(m.exogenous_shocks), by = x -> x[2])]
 
-obs_labels = [key for (key, _) in sort(collect(m.observables), by = x -> x[2])]
 
-combined = OrderedDict{Symbol, Int64}()
+combined = DSGE.OrderedDict{Symbol, Int64}()
 # First insert all entries from endogenous_states
 for (k, v) in m.endogenous_states
 combined[k] = v
@@ -116,45 +66,73 @@ for (k, v) in m.endogenous_states_augmented
 combined[k] = v
 end
 state_labels = [key for (key, _) in sort(collect(combined), by = x -> x[2])]
+pseudo_labels = [key for (key, _) in sort(collect(m.pseudo_observables), by = x -> x[2])]
 
+system = DSGE.compute_system(m)
+
+states_df = Dict{Symbol, DataFrame}()
+shocks_df = Dict{Symbol, DataFrame}()
+pseudo_df = Dict{Symbol, DataFrame}()
+
+smoother = :durbin_koopman #:hamilton, :koopman, :carter_kohn, 
+m <= DSGE.Setting(:forecast_smoother, smoother)
+
+states[smoother], shocks[smoother], pseudo[smoother] =
+        DSGE.smooth(m, df, system; draw_states = false)
+
+dates = df.date[end-size(states[smoother],2)+1:end]
+mat = states[smoother]'  # transpose to 259×91
+states_df[smoother] = DataFrame(hcat(dates, mat), [:date; state_labels])
+
+mat = shocks[smoother]'  # transpose to 259×29
+shocks_df[smoother] = DataFrame(hcat(dates, mat), [:date; shock_labels])
+
+mat = pseudo[smoother]'  # transpose to 259×22
+pseudo_df[smoother] = DataFrame(hcat(dates, mat), [:date; pseudo_labels])
+
+# Collect from the dataframe the respective shocks
+
+#shocks_df[smoother][:, [:date; :b_liqtil_sh; :b_liqp_sh; :b_safetil_sh; :b_safep_sh]]
+privilege_shock_names = [:b_liqtil_sh; :b_liqp_sh; :b_safetil_sh; :b_safep_sh]
+privilege_shock_vals  = convert(Matrix,shocks_df[smoother][:, [:b_liqtil_sh; :b_liqp_sh; :b_safetil_sh; :b_safep_sh]])
  
-#  @inline Φ(s_t1::Vector{S}, ϵ_t::Vector{S}) = TTT*s_t1 + RRR*ϵ_t + CCC
-#  @inline Ψ(s_t::Vector{S}) = ZZ*s_t + DD
+horizon= size(privilege_shock_vals,1)
+nshocks = size(system[:RRR], 2)
+nstates = size(system[:TTT], 1)
+s_0 = zeros(nstates)
+shocks = zeros(nshocks, horizon)
 
-#  # Define shock and measurement error distributions
-#  nshocks = size(QQ, 1)
-#  nobs    = size(EE, 1)
-#  F_ϵ = Distributions.MvNormal(zeros(nshocks), QQ)
-#  F_u = Distributions.MvNormal(zeros(nobs),    EE)
+m1 = Model1010("ss20")
+mode_file = joinpath("dsge/output_data/m1010/ss20/estimate/raw" ,  "paramsmode_vint=161223.h5")
+specify_mode!(m1, mode_file)
+system_US = DSGE.compute_system(m1)
 
-#  return Φ, Ψ, F_ϵ, F_u
-
-system = compute_system(m)
-# Unpack system
-TTT    = system[:TTT]
-RRR    = system[:RRR]
-CCC    = system[:CCC]
-QQ     = system[:QQ]
-ZZ     = system[:ZZ]
-DD     = system[:DD]
-EE     = system[:EE]
-
-
-folder_path = joinpath(basepath, "counterfactual", "rstarobs_plus_convyieldobs")
-
-# Create the folder if it doesn't exist
-if !isdir(folder_path)
-    mkdir(folder_path)
+for (i, shock_name) in enumerate(privilege_shock_names)
+    shock_ind_US = m1.exogenous_shocks[shock_name]
+    shocks[shock_ind_US, :] .= privilege_shock_vals[:, i]
 end
-# Save matrices as CSV files
-CSV.write("counterfactual/rstarobs_plus_convyieldobs/TTT.csv",DataFrame(TTT,state_labels))
-CSV.write("counterfactual/rstarobs_plus_convyieldobs/RRR.csv",DataFrame(RRR,shock_labels))
-CSV.write("counterfactual/rstarobs_plus_convyieldobs/CCC.csv",DataFrame(CCC',state_labels))
-CSV.write("counterfactual/rstarobs_plus_convyieldobs/QQ.csv",DataFrame(QQ,shock_labels))
-CSV.write("counterfactual/rstarobs_plus_convyieldobs/ZZ.csv",DataFrame(ZZ',obs_labels))
-CSV.write("counterfactual/rstarobs_plus_convyieldobs/DD.csv",DataFrame(DD',obs_labels))
-CSV.write("counterfactual/rstarobs_plus_convyieldobs/EE.csv",DataFrame(EE,obs_labels))
 
-# produce LaTeX tables of parameter moments
-# moment_tables(m)
+# Compute IRF for a unit shock at time t for the specified shock
+        states_privilege, obs_privilege, pseudo_privilege = forecast(system, s_0, shocks)
+               
 
+using Plots
+p1 = plot(dates,obs_privilege[m.observables[:obs_nominalrate],:],title="Policy rate")
+plot!(dates,zeros(horizon,1),lc=:black,lw=2,label="")
+p2 = plot(dates,obs_privilege[m.observables[:obs_gdpdeflator],:],title="Inflation")
+plot!(dates,zeros(horizon,1),lc=:black,lw=2,label="")
+p3 = plot(dates,obs_privilege[m.observables[:obs_gdp],:],title="Output")#
+plot!(dates,zeros(horizon,1),lc=:black,lw=2,label="")
+p4 = plot(dates,pseudo_privilege[m.pseudo_observables[:Forward5YearRealNaturalRate],:],title="r* (Forward 5-year real natural rate)")#
+plot!(dates,zeros(horizon,1),lc=:black,lw=2,label="")
+# p6 = plot(1:horizon,pseudo[m.pseudo_observables[:RealNaturalRate],:, m.exogenous_shocks[:rm_sh]],title="Real natural rate")#
+# plot!(zeros(horizon,1),lc=:black,lw=2,label="")
+
+plot(p1, p2, p3, p4,layout=(2,2), legend=false)
+plot!(size=(960,540))
+if use_FG_in_EA 
+    savefig( "Main results/Exorbitant privilege.pdf")   # saves the plot from p as a .pdf vector graphic
+
+else
+    savefig( "Main results/Exorbitant privilege without FG shocks in EA.pdf")   # saves the plot from p as a .pdf vector graphic
+end
